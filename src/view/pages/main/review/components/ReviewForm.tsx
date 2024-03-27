c3b2800177c12336cd8ec28a597a2c48afcf57b0
Luxuni <3422056558@qq.com>
2024-03-27 14:44:16 +0800

    feat: formRender

--- src/view/pages/main/review/components/ReviewForm.tsx
+++ src/view/pages/main/review/components/ReviewForm.tsx
@@ -1,31 +1,26 @@
-import { Card, Form, Input, Select } from 'antd';
+import { Card } from 'antd';
+import FormRender, { useForm } from 'form-render';
 import { Resource } from '../../../../resource';
 import { getUserList } from '../../../../services';
-
-const { TextArea } = Input;
+import schemaCreater from './schema/mainForm';
 
 const resource = new Resource(getUserList());
 const ReviewForm: React.FC = () => {
   const userList = resource.read();
+  const form = useForm();
+
+  const onFinish = (formData: any) => {
+    console.log('formData:', formData);
+  };
   return (
     <Card bordered={false}>
-      <Form labelCol={{ span: 3 }} labelAlign="left" layout="horizontal">
-        <Form.Item label="评审意见">
-          <TextArea rows={4} />
-        </Form.Item>
-        <Form.Item label="问题类型">
-          <Input />
-        </Form.Item>
-        <Form.Item label="作者">
-          <Select>
-            {userList?.map((v, i) => (
-              <Select.Option value={v.userId} key={v.userId}>
-                {v.name}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-      </Form>
+      <FormRender
+        form={form}
+        schema={schemaCreater(userList ?? [])}
+        onFinish={onFinish}
+        maxWidth={360}
+        footer={true}
+      />
     </Card>
   );
 };
---
+++ src/view/pages/main/review/components/schema/mainForm.ts
@@ -0,0 +1,77 @@
+const mainFormCreate = (
+  authorOptions: {
+    name: string;
+    userId: number;
+  }[],
+) => {
+  return {
+    type: 'object',
+    properties: {
+      view: {
+        title: '评审意见',
+        type: 'string',
+        props: {
+          placeholder: '请输入评审意见',
+          allowClear: true,
+        },
+        tooltip: {
+          title: '请输入评审意见',
+        },
+        required: true,
+        message: {
+          required: '请输入评审意见',
+        },
+        widget: 'input',
+      },
+      type: {
+        title: '问题类型',
+        type: 'string',
+        props: {
+          options: [
+            {
+              label: 'A',
+              value: 'A',
+            },
+            {
+              label: 'B',
+              value: 'B',
+            },
+          ],
+          placeholder: '请选择问题类型',
+        },
+        tooltip: {
+          title: '请选择问题类型',
+        },
+        required: true,
+        message: {
+          required: '请选择问题类型',
+        },
+        widget: 'select',
+      },
+      author: {
+        title: '作者',
+        type: 'number',
+        props: {
+          options: authorOptions.map((item) => ({
+            label: item.name,
+            value: item.userId,
+          })),
+          placeholder: '请选择',
+        },
+        description: '',
+        tooltip: {
+          title: '请选择作者',
+        },
+        required: true,
+        message: {
+          required: '请选择作者',
+        },
+        widget: 'select',
+      },
+    },
+    displayType: 'column',
+    column: 1,
+    maxWidth: '100%',
+  };
+};
+export default mainFormCreate;
